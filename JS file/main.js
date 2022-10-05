8dc3c4c42de4fad0ddb311e2fabe811fbd0bb89c
parvesh <vparvesh830@gmail.com>
2022-10-05 21:52:49 +0530

    working on shadow effect

--- JS file/main.js
+++ JS file/main.js
@@ -1,11 +1,20 @@
+/******* Side Nav Bar *******/
 
-//search bar
-let search = document.getElementById("search");
-let searchOpt = document.getElementById("search-option");
+let showSideBar = document.getElementById("show-side-bar");
+let hMenuContainer = document.getElementById("hmenu-container");
+let hMenuBackground = document.getElementById("hmenu-background")
+let hMenu = document.getElementById("hmenu");
+let xMark = hMenu.children[2];
 
-search.addEventListener("focus", () => {
-    searchOpt.style.border = "3px solid orange";
-})
-search.addEventListener("blur", () => {
-    searchOpt.style.border = "3px solid transparent";
+showSideBar.addEventListener("click", () => {
+    hMenu.style.transform = "translateX(0)";
+    xMark.style.display = "block";
+    hMenuContainer.style.visibility = "visible";
+    hMenuBackground.style.opacity = "1";
+});
+xMark.addEventListener("click", () => {
+    hMenu.style.transform = "translateX(-100%)";
+    xMark.style.display = "none";
+    hMenuContainer.style.visibility = "hidden";
+    hMenuBackground.style.opacity = "0";
 });
